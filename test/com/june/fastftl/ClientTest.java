0e47d668e4b1cce38ff1c5ede4129fc8722d0a58
hzxujunyu <543717080@qq.com>
2017-02-13 15:01:21 +0800

    ClientTest

--- test/com/june/fastftl/ClientTest.java
+++ test/com/june/fastftl/ClientTest.java
@@ -4,7 +4,10 @@ package com.june.fastftl;
  * Created by june on 2017/1/22.
  */
 public class ClientTest {
-    public static void main(String[] args){
-        new Client().request(8005, "{\"template\":\"test.ftl\",\"data\":{\"a\": \"Hello\"}}");
+    public static void main(String[] args) {
+        int i = 0;
+        while (i++ < 100) {
+            new Client().request(8005, "{\"template\":\"test.ftl\",\"data\":{\"a\": \"Hello\"}}");
+        }
     }
 }
