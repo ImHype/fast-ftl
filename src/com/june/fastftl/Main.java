654f64ddcd4b96197432a07f5fa9c530237211da
ziyuan <ziyuan@2dfire.com>
2017-03-28 20:57:21 +0800

    不可变处理

--- src/com/june/fastftl/Main.java
+++ src/com/june/fastftl/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Config config = new Config(args);
-        Render render = new Render(config.getRoot());
+        final Render render = new Render(config.getRoot());
 
         Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
             @Override
