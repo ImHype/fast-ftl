03d1147624700199b552032223952a3b842f69c4
ziyuan <ziyuan@2dfire.com>
2017-03-28 20:55:36 +0800

    异步掉client接入的过程

--- src/com/june/fastftl/Main.java
+++ src/com/june/fastftl/Main.java
@@ -1,16 +1,28 @@
 package com.june.fastftl;
-import com.june.fastftl.model.Config;
+
 import com.june.fastftl.impl.Render;
 import com.june.fastftl.impl.Server;
+import com.june.fastftl.model.Config;
+
 /**
  * Created by june on 2017/1/22.
  */
 public class Main {
-    public Main () {}
-    public static void main (String[] args) throws Exception {
+    public Main() {
+    }
+
+    public static void main(String[] args) throws Exception {
         Config config = new Config(args);
         Render render = new Render(config.getRoot());
+
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ThreadManager.INSTANCE.shutDown();
+            }
+        }));
+
         System.out.printf("[D] built in " + config.getPort());
-        new Server(config.getPort(), render);
+        new Server(config.getPort(), render).start();
     }
 }
