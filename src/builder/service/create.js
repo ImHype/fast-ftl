c1f191ba50c51b924f19d718d94ffd4c08d53e46
君羽 <ImHype@users.noreply.github.com>
2017-04-02 08:27:18 +0800

    Update create.js

--- src/builder/service/create.js
+++ src/builder/service/create.js
@@ -7,18 +7,15 @@ function serviceBuildPromise(service) {
     return new Promise(resolve => {
         service.stdout.on('data', data => {
             if (~data.indexOf('built')) {
-                setTimeout(()=>{
-                    resolve();
-                }, 200);
+                setTimeout(resolve, 200);
             }
         });
     });
 }
 
 function createService(options) {
-    const jarFile = resolve(__dirname, '../../../lib/Fast-FTL.jar');
-    // console.log(options)
-    return invokeJar(jarFile, [options]);
+    const jarFile = resolve(__dirname, '../../../lib/Fast-FTL.jar')
+    return invokeJar(jarFile, [JSON.stringify(options)]);
 }
 
 /**
@@ -26,7 +23,7 @@ function createService(options) {
  * port
  */
 export default function (options) {
-    const service = createService(JSON.stringify(options));
+    const service = createService(options);
     killServiceWhenKillCurrent(service);
     logServiceError(service);
     return serviceBuildPromise(service);
